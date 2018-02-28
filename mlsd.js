cfe05162d8979875bc9d1d08a7538f5167f0d19c
Ruslan Komarichev <meowhellyeah@gmail.com>
2018-03-01 00:28:34 +0300

    Прототипы, генерация страницы upd, чтение структуры ref #1 - созданы прототипы элементов матрицы; - оптимизирован код генерации страницы; - структура закладок считывается из локального хранилища (пока не записывается -> чтение возвращает значение по умолчанию) ref #1.

--- mlsd.js
+++ mlsd.js
@@ -1,64 +1,127 @@
-/*Описание прототипов*/
-//Драсти
-/*Окончание описания прототипов*/
-
-window.onload = function() {
-    buildPage(3, 3, 0, 0);
-}
-
-const bgTypes = {
+/*Описание констант*/
+const BgType = {
     DEFAULT: 0,
     SOLID: 1,
     IMAGE: 2
 }
-Object.freeze(bgTypes);
+Object.freeze(BgType);
 
-function buildPage(width, height, bgtype, bgdata) {
-    bgtype = bgTypes.IMAGE;
-    bgdata = "https://pp.userapi.com/c621515/v621515823/7470c/gUhs_I6VmrM.jpg";
+const ElementType = {
+    EMPTY: 0,
+    BOOKMARK: 1,
+    FOLDER: 2
+}
+Object.freeze(ElementType);
 
-    var bg;
-    switch (bgtype) {
-        case bgTypes.DEFAULT:
-            bg = 'bgcolor="#F9F9FA"';
+const StrongString = {
+    SEPARATOR: "_",
+    ELEMENT: "el",
+    CAPTION: "cap"
+}
+Object.freeze(StrongString);
+
+const DEFAULT_BGCOLOR = "#F9F9FA";
+/*Окончание описания констант*/
+
+/*Описание прототипов*/
+var Element = function(code) {
+    this.type = ElementType.EMPTY;
+    this.code = code;
+}
+Element.prototype.whoAreYou = function() {
+    console.log("I am " + this.type + " #" + this.code + ", ")
+}
+Element.prototype.generateHtml = function() {
+    return '<div class="element" id="' + StrongString.ELEMENT + StrongString.SEPARATOR + this.code + '">' +
+            '<div class="elementHeader"><img src="http://google.com/favicon.ico">' +
+            '<label class="elementCaption" id="' + StrongString.CAPTION + StrongString.SEPARATOR + this.code + '">' +
+            'Подпись подпись подпись подпись подпись подпись подпись подпись подпись подпись</label>' +
+            '<img class="elementButton" src="icons/refresh.png">' +
+            '<img class="elementButton" src="icons/edit.png">' +
+            '<img class="elementButton" src="icons/delete.png"></div></div>';
+}
+Element.prototype.bindHtml = function() {
+    //this????
+}
+
+var Bookmark = function(code) {
+    Element.apply(this, [code]);
+    this.type = ElementType.BOOKMARK;
+}
+Bookmark.prototype = Object.create(Element.prototype);
+Bookmark.prototype.constructor = Bookmark;
+
+var Folder = function(code, rows, cols) {
+    Element.apply(this, [code]);
+    this.type = ElementType.FOLDER;
+    this.rows = rows;
+    this.cols = cols;
+    this.bgtype = BgType.DEFAULT;
+    this.bgdata = null;
+
+    let square = rows * cols;
+    this.elements = new Array(square);
+    for (let i = 0; i < square; ++i) {
+        this.elements[i] = new Element(i + 1);
+    }
+}
+Folder.prototype = Object.create(Element.prototype);
+Folder.prototype.constructor = Folder;
+/*Окончание описания прототипов*/
+
+var currPath;
+
+window.onload = function() {
+    currPath = "";
+    browser.storage.local.get({structure: new Folder(0, 3, 3)}).then(onStructureLoaded, onStructireLoadFailed);
+}
+
+function onStructureLoaded(results) {
+    console.log("Results: ");
+    for (let key in results) {
+        console.log(results[key]);
+    }
+    buildPage(results.structure);
+}
+
+function onStructireLoadFailed(error) {
+    console.log("Fail: " + error);
+}
+
+function buildPage(folder) {
+    folder.bgtype = BgType.IMAGE;
+    folder.bgdata = "https://pp.userapi.com/c621515/v621515823/7470c/gUhs_I6VmrM.jpg";
+
+    switch (folder.bgtype) {
+        case BgType.DEFAULT:
+            document.body.style.backgroundColor = DEFAULT_BGCOLOR;
+            document.body.style.backgroundImage = "";
             break;
-        case bgTypes.SOLID:
-            bg = 'bgcolor="' + bgdata + '"';
+        case BgType.SOLID:
+            document.body.style.backgroundColor = folder.bgdata;
+            document.body.style.backgroundImage = "";
             break;
-        case bgTypes.IMAGE:
-            bg = 'background="' + bgdata + '"';
+        case BgType.IMAGE:
+            document.body.style.backgroundColor = "";
+            document.body.style.backgroundImage = "url('" + folder.bgdata + "')";
             break;
     }
 
-    var newBody = '<body ' + bg + '>\n\t<table cellspacing="10px">\n';
-    for (i = 0; i < height; ++i) {
-        newBody = newBody + '<tr>\n';
-        for (j = 0; j < width; ++j) {
-            newBody = newBody + '\t<td>\n';
-            newBody = newBody + '\t\t' + buildHtmlElement(i, j) + '\n';
-            newBody = newBody + '\t</td>\n';
+    let grid = document.getElementById("grid");
+    grid.innerHTML = "";
+    for (let i = 0; i < folder.rows; ++i) {
+        let row = grid.insertRow(i);
+        for (let j = 0; j < folder.cols; ++j) {
+            let cell = row.insertCell(j);
+            let element = folder.elements[folder.cols * i + j];
+            cell.innerHTML = Element.prototype.generateHtml.call(element);
+            assignJsElement(element.code); //TODO: переписать
         }
-        newBody = newBody + '</tr>\n';
     }
-
-    replaceBody(newBody);
 }
 
-function buildHtmlElement(x, y) {
-    var code = '<div class="element" id="el_' + x + '_' + y + '">' +
-	        '<div class="elementHeader"><img src="http://google.com/favicon.ico">' +
-	        '<label class="elementCaption" id="caption_' + x + '_' + y + '">' +
-	        'Подпись подпись подпись подпись подпись подпись подпись подпись подпись подпись</label>' +
-	        '<img class="elementButton" src="icons/refresh.png">' +
-	        '<img class="elementButton" src="icons/edit.png">' +
-	        '<img class="elementButton" src="icons/delete.png"></div></div>';
-    return code;
-}
-
-function replaceBody(newBody) {
-    document.body.parentNode.innerHTML = '<!DOCTYPE html>\n<html>\n\n<head>\n\t' +
-	        '<meta charset="UTF-8">\n\t<title>Multilevel Speed Dial</title>\n\t' +
-	        '<link rel="stylesheet" type="text/css" href="style.css">\n\t' +
-	        '<script type="text/javascript" src="mlsd.js"></script>\n</head>\n' +
-	        newBody + '\n</html>\n';
+//TODO: внести в Element
+function assignJsElement(code) {
+    var element = new Element(code);
+    document.getElementById(StrongString.ELEMENT + StrongString.SEPARATOR + code).onclick = element.whoAreYou.bind(element);
 }
